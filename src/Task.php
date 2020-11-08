57a1c0373267201c9787b7c04355f05ba00cb8a8
MGatner <mgatner@icloud.com>
2020-11-08 03:06:28 +0000

    Apply styles to Task

--- src/Task.php
+++ src/Task.php
@@ -6,112 +6,113 @@
  * Represents a single task that should be scheduled
  * and run periodically.
  */
+
 abstract class Task
 {
-    use FrequenciesTrait;
+	use FrequenciesTrait;
 
-    /**
-     * The command, shell command, or Closure
-     * that should be ran.
-     *
-     * @var mixed
-     */
-    protected $task;
+	/**
+	 * The command, shell command, or Closure
+	 * that should be ran.
+	 *
+	 * @var mixed
+	 */
+	protected $task;
 
-    /**
-     * The task type, either 'callable', 'command', or 'shell'
-     *
-     * @var string
-     */
-    protected $taskType;
+	/**
+	 * The task type, either 'callable', 'command', or 'shell'
+	 *
+	 * @var string
+	 */
+	protected $taskType;
 
-    /**
-     * The timezone the event should be evaluated in.
-     *
-     * @var string
-     */
-    protected $timezone;
+	/**
+	 * The timezone the event should be evaluated in.
+	 *
+	 * @var string
+	 */
+	protected $timezone;
 
-    /**
-     * If not empty, lists the allowed environments
-     * this can run in.
-     *
-     * @var string[]
-     */
-    protected $environments = [];
+	/**
+	 * If not empty, lists the allowed environments
+	 * this can run in.
+	 *
+	 * @var string[]
+	 */
+	protected $environments = [];
 
-    /**
-     * @param mixed $task
-     * @param string $taskType
-     */
-    public function __construct($task, string $taskType)
-    {
-        $this->task     = $task;
-        $this->taskType = $taskType;
-    }
+	/**
+	 * @param mixed  $task
+	 * @param string $taskType
+	 */
+	public function __construct($task, string $taskType)
+	{
+		$this->task     = $task;
+		$this->taskType = $taskType;
+	}
 
-    abstract public function run();
+	abstract public function run();
 
-    /**
-     * Determines whether this task should be run now
-     * according to its schedule, timezone, and environment.
-     *
-     * @return bool
-     */
-    public function shouldRun(): bool
-    {
-    }
+	/**
+	 * Determines whether this task should be run now
+	 * according to its schedule, timezone, and environment.
+	 *
+	 * @return boolean
+	 */
+	public function shouldRun(): bool
+	{
+	}
 
-    /**
-     * Returns the saved task.
-     *
-     * @return mixed
-     */
-    public function getTask()
-    {
-        return $this->task;
-    }
+	/**
+	 * Returns the saved task.
+	 *
+	 * @return mixed
+	 */
+	public function getTask()
+	{
+		return $this->task;
+	}
 
-    /**
-     * Returns the saved task type.
-     *
-     * @return string
-     */
-    public function getType(): string
-    {
-        return $this->taskType;
-    }
+	/**
+	 * Returns the saved task type.
+	 *
+	 * @return string
+	 */
+	public function getType(): string
+	{
+		return $this->taskType;
+	}
 
-    /**
-     * Restricts this task to run within only
-     * specified environements.
-     *
-     * @param mixed ...$environments
-     *
-     * @return $this
-     */
-    protected function environments(...$environments)
-    {
-        $this->environments = $environments;
+	/**
+	 * Restricts this task to run within only
+	 * specified environements.
+	 *
+	 * @param mixed ...$environments
+	 *
+	 * @return $this
+	 */
+	protected function environments(...$environments)
+	{
+		$this->environments = $environments;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Checks if it runs within the specified environment.
-     *
-     * @param string $environment
-     *
-     * @return bool
-     */
-    protected function runsInEnvironment(string $environment): bool
-    {
-        // If nothing specified should run anywhere
-        if (empty($this->environments))
-        {
-            return true;
-        }
+	/**
+	 * Checks if it runs within the specified environment.
+	 *
+	 * @param string $environment
+	 *
+	 * @return boolean
+	 */
+	protected function runsInEnvironment(string $environment): bool
+	{
+		// If nothing specified should run anywhere
+		if (empty($this->environments))
+		{
+			return true;
+		}
 
-        return in_array($environment, $this->environments);
-    }
+		return in_array($environment, $this->environments);
+	}
 }
